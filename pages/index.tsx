d40a7a9cade2d9e5f9927ef61a1cd74cc508f532
KMoussa33 <kelsey.moussa@gmail.com>
2023-09-05 21:06:20 -0400

    Header, hero, and background updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Kelsey's Portfolio</title>
       </Head>
       
-      {/* Header */}
       <Header />
+
       {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
 
       {/* About */}
 
@@ -24,6 +28,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-    </>
+    </div>
   )
 }
