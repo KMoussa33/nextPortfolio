2bdbc76491e868362c102dc91f15cffcda61a1d5
KMoussa33 <kelsey.moussa@gmail.com>
2023-09-05 21:20:54 -0400

    background circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -5,12 +5,23 @@ type Props = {}
 
 export default function BackgroundCircles({}: Props) {
   return (
-    <div className='relative flex justify-center items-center'>
+    <motion.div 
+    initial={{
+        opacity: 0,
+    }}
+    animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+    }}
+    transition={{
+        duration: 2.5,
+    }}
+    className='relative flex justify-center items-center'>
         <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
         <div className='absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52' />
         <div className='absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52' />
         <div className='rounded-full border border-[#2e8544] opacity-20 h-[650px] w-[650px] mt-52 animate-pulse' />
         <div className='rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52' />
-    </div>
+    </motion.div>
   );
 }
